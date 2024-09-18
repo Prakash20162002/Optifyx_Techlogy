9d2024fa3487ab709d626090ccbdfd36aaabb922
Prakash <Prakash.com>
2024-09-19 01:34:22 +0530

    Finished Android Project TASK_1

--- app/src/main/java/com/example/todolist/RecyclerViewTouchHelper.java
+++ app/src/main/java/com/example/todolist/RecyclerViewTouchHelper.java
@@ -3,12 +3,14 @@ package com.example.todolist;
 import android.content.DialogInterface;
 import android.graphics.Canvas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.annotation.NonNull;
+
 import com.example.todolist.Adapter.todoadapter;
-import androidx.core.content.ContextCompat;
+
 import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
 public class RecyclerViewTouchHelper extends ItemTouchHelper.SimpleCallback{
 
@@ -60,8 +62,10 @@ public class RecyclerViewTouchHelper extends ItemTouchHelper.SimpleCallback{
     @Override
     public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
         new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
-                .addBackgroundColor(ContextCompat.getColor(adapter.getContext(), R.color.my_background)) // Fixed method name
-                .addActionIcon(R.drawable.delete) // Fixed method name and syntax
+                .addSwipeLeftBackgroundColor(ContextCompat.getColor(adapter.getContext(),R.color.green)) // Fixed method name
+                .addSwipeLeftActionIcon(R.drawable.edit) // Fixed method name and syntax
+                .addSwipeRightBackgroundColor(ContextCompat.getColor(adapter.getContext(),R.color.red))
+                .addSwipeRightActionIcon(R.drawable.delete)
                 .create()
                 .decorate();
 
